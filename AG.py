ed1ba2b11f42b0e702a9cdde59c0ea06bf7f2a4f
pedrohba1 <pedro.almeida@ufu.br>
2021-09-12 17:27:45 -0300

    geraçao do primeiro cromossomo aleatorio

--- AG.py
+++ AG.py
@@ -1,13 +1,27 @@
 import csv
+import random
 
 filmesDict = {}
 with open('filmes.csv', newline='') as csvfile:
     filmes = csv.reader(csvfile, delimiter=',', quotechar='|')
     for idx, row in enumerate(filmes): 
+        # print(idx, row[4])
         filmesDict[idx] = row[4]
 
-
-print(filmesDict)
-
+minutesPerDay = 240
 
 
+
+# seleciona filmes aleatórios e cria os primeiros cromossomos aleatoriamente.
+# O primeiro filme a ser assistindo está no índice 0, o segundo no índice 1, e por aí vai...
+# a soma de tempo dos filmes não pode ser maior que 240.
+
+toRandomizeDict = filmesDict
+
+cromossome = []
+while(len(list(toRandomizeDict.keys())) != 0 ):
+    randMovieKey = random.choice(list(filmesDict.keys()))
+    toRandomizeDict.pop(randMovieKey)
+    cromossome.append(randMovieKey)
+
+print(cromossome)
